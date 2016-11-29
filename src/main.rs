089b4b97532029ab3da64a1f268c3ec1263bfa7a
Sier Violencia <sierisimo@gmail.com>
2016-11-28 20:15:25 -0600

    Algorith-mx in rust

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("Hello, world!");
+
+    let array = vec![12, 4, 2, 7, 9, 27, 1, 3, 8, 6, 5, 0];
+    let number = 7;
+    bigO(array, number);
+}
+
+fn bigO(arrayOfInts: Vec<u32>, numberToSearch: u32) {
+    let mut idx_start = 0;
+    let mut idx_end = arrayOfInts.len();
+
+    while(idx_start != idx_end){
+        
+    }
+}
